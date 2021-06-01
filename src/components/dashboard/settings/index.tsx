8df1db4ad3c5a8f32d3f9df8a5834eddc04900e2
sanu23kumar <sanu23kumar@gmail.com>
2021-06-02 04:43:37 +0530

    feat: fixes #11

--- src/components/dashboard/settings/index.tsx
+++ src/components/dashboard/settings/index.tsx
@@ -1,22 +1,75 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import React, { useState } from 'react';
+import {
+  ActivityIndicator,
+  Pressable,
+  SafeAreaView,
+  Text,
+  ToastAndroid,
+  View,
+} from 'react-native';
 import strings from '../../../assets/strings';
+import { getDistrict, suggestDistricts } from '../../../services';
+import { StatesModel } from '../../../services/models/districts';
+import { useDistrictsStore, useUserStore } from '../../../services/stores';
 import VtHeader from '../../common/header';
 import useStyle from './styles';
 
 const Settings = () => {
   const styles = useStyle();
+  const [isRefreshingDistricts, setIsRefreshingDistricts] = useState(false);
+  const {
+    data: {
+      location: { name },
+    },
+  } = useUserStore();
+  const {
+    districtsData: { states },
+    setDistrictsData,
+  } = useDistrictsStore();
+
+  const userState = suggestDistricts(name, states)[0];
+
+  const onPressRefreshStates = async () => {
+    setIsRefreshingDistricts(true);
+    const districtsResponse = await getDistrict(userState?.state_id);
+    console.log(districtsResponse);
+    if (districtsResponse?.districts?.length > 0) {
+      const newStatesData: StatesModel[] = JSON.parse(JSON.stringify(states));
+      newStatesData.forEach(state => {
+        if (state.state_id === userState?.state_id) {
+          state.districts = districtsResponse.districts;
+        }
+      });
+      setDistrictsData({ states: newStatesData });
+      ToastAndroid.show(
+        'Districts updated for ' + userState.state_name,
+        ToastAndroid.SHORT,
+      );
+    } else {
+      ToastAndroid.show(
+        'Update failed, please try sometime later',
+        ToastAndroid.SHORT,
+      );
+    }
+    setIsRefreshingDistricts(false);
+  };
+
   return (
     <SafeAreaView style={styles.parent}>
-      <VtHeader title={strings.dashboard.settings.header}>
-        <Icon
-          name="add"
-          color={styles.iconStyle.color}
-          size={28}
-          style={styles.iconStyle}
-        />
-      </VtHeader>
+      <VtHeader title={strings.dashboard.settings.header} />
+      {userState ? (
+        <Pressable onPress={onPressRefreshStates} style={styles.refetchParent}>
+          <View>
+            <Text style={styles.refetchText}>Refetch districts for</Text>
+            <Text style={styles.refetchStateText}>{userState.state_name}</Text>
+          </View>
+          <ActivityIndicator
+            animating={isRefreshingDistricts}
+            color={styles.refetchIconStyle.color}
+            size={24}
+          />
+        </Pressable>
+      ) : null}
     </SafeAreaView>
   );
 };
--- src/components/dashboard/settings/styles.ts
+++ src/components/dashboard/settings/styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import fonts from '../../../assets/fonts';
 import useVtTheme from '../../../assets/theme/useVtTheme';
 
 const useStyle = () => {
@@ -13,6 +14,27 @@ const useStyle = () => {
     parent: {
       flex: 1,
       backgroundColor: colors.BACKGROUND,
+    },
+    refetchParent: {
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    refetchText: {
+      fontSize: 12,
+      fontFamily: fonts.MEDIUM,
+      color: colors.TEXT_DISABLED,
+      textTransform: 'uppercase',
+    },
+    refetchStateText: {
+      fontSize: 16,
+      fontFamily: fonts.REGULAR,
+      color: colors.TEXT,
+    },
+    refetchIconStyle: {
+      color: colors.PRIMARY,
     },
   });
 };
