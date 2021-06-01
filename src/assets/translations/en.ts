295ee61183b34044c0d68dd42d55611483d278ee
Paras Lehana <paraskumarlehana602@gmail.com>
2021-06-01 22:42:34 +0530

    FEAT Add Strings for EN and HI

--- src/assets/translations/en.ts
+++ src/assets/translations/en.ts
@@ -2,6 +2,37 @@ import { TranslationModel } from './model';
 
 const en: TranslationModel = {
   HOME_SCREEN_TITLE: 'Centers',
+  NOTIFICATION_SCREEN_TITLE: 'Notifications',
+  SETTINGS_SCREEN_TITLE: 'Settings',
+  FILTER_TEXT: 'Filter',
+  FILTER_ALL_TEXT: 'All',
+  FILTER_VACCINE_TEXT: 'Vaccine',
+  FILTER_VACCINE_SPUTNIK_TEXT: 'Sputnik',
+  FILTER_VACCINE_COVISHIELD_TEXT: 'Covishield',
+  FILTER_VACCINE_COVAXIN_TEXT: 'Covaxin',
+  FILTER_AGE_TEXT: 'Age',
+  FILTER_AGE_18_PLUS_TEXT: '18+',
+  FILTER_AGE_45_PLUS_TEXT: '45+',
+  FILTER_COST_TEXT: 'Cost',
+  FILTER_COST_FREE_TEXT: 'Free',
+  FILTER_COST_PAID_TEXT: 'Paid',
+  FILTER_DOSE_TEXT: 'Dose Availability',
+  FILTER_DOSE_FIRST_TEXT: '1st',
+  FILTER_DOSE_SECOND_TEXT: '2nd',
+  FILTER_APPLY_TEXT: 'Apply',
+  FILTER_RESET_TEXT: 'Reset',
+
+  HOME_SEARCH_TEXT: 'Search by District or Pincode',
+  HOME_SCREEN_AVAILABLE_TEXT: 'available',
+  BOOK_NOW_TEMPLATE: 'Book now for $',
+  HOME_SCREEN_NOTIFY_ME_TEXT: 'Notify me when available',
+  HOME_ZERO_RESULT_TEXT: 'No centers found',
+  HOME_ZERO_RESULT_CLEAR_FILTERS_TEXT: 'Clear Filters',
+
+  NOTIFICATIONS_ADD: 'Add Notification',
+  NOTIFICATION_DATE: 'Due Date',
+  NOTIFICATION_LOCATION: 'District or Pincode',
+  NOTIFICATION_TITLE: 'Notification Title',
 };
 
 export default en;
---
+++ src/assets/translations/hi.ts
@@ -0,0 +1,39 @@
+import { TranslationModel } from './model';
+
+const hi: TranslationModel = {
+  HOME_SCREEN_TITLE: 'केन्द्र',
+  NOTIFICATION_SCREEN_TITLE: 'सूचनाएं',
+  SETTINGS_SCREEN_TITLE: 'सेटिंग्स',
+  FILTER_TEXT: 'छानिये',
+  FILTER_ALL_TEXT: 'सभी',
+  FILTER_VACCINE_TEXT: 'टीका',
+  FILTER_VACCINE_SPUTNIK_TEXT: 'स्पूतनिक',
+  FILTER_VACCINE_COVISHIELD_TEXT: 'कोविशील्ड',
+  FILTER_VACCINE_COVAXIN_TEXT: 'कोवैक्सीन',
+  FILTER_AGE_TEXT: 'उम्र',
+  FILTER_AGE_18_PLUS_TEXT: 'अठारह से अधिक',
+  FILTER_AGE_45_PLUS_TEXT: 'पैंतालीस से अधिक',
+  FILTER_COST_TEXT: 'कीमत',
+  FILTER_COST_FREE_TEXT: 'मुफ़्त',
+  FILTER_COST_PAID_TEXT: 'सशुल्क',
+  FILTER_DOSE_TEXT: 'खुराक उपलब्धता',
+  FILTER_DOSE_FIRST_TEXT: 'पहला',
+  FILTER_DOSE_SECOND_TEXT: 'दूसरा',
+  FILTER_APPLY_TEXT: 'लागू करें',
+  FILTER_RESET_TEXT: 'साफ़ करें',
+
+  HOME_SEARCH_TEXT: 'जिला या पिनकोड द्वारा खोजें',
+  HOME_SCREEN_AVAILABLE_TEXT: 'उपलब्ध है',
+  BOOK_NOW_TEMPLATE: '$ में अभी बुक करें',
+  HOME_SCREEN_NOTIFY_ME_TEXT: 'उपलब्ध होने पर सूचित करें',
+  HOME_ZERO_RESULT_TEXT: 'कोई केंद्र नहीं मिला',
+  HOME_ZERO_RESULT_CLEAR_FILTERS_TEXT: 'छन्नी साफ़ करें',
+
+  NOTIFICATIONS_ADD: 'नोटिफिकेशन से सूचित करें',
+  NOTIFICATION_DATE: 'नीयत तारीख़',
+  NOTIFICATION_LOCATION: 'जिला या पिनकोड',
+  NOTIFICATION_TITLE: 'नोटिफिकेशन का नाम',
+
+};
+
+export default hi;
